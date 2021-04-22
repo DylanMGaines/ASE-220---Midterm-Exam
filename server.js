8aac0938a449e68396db3c405f0bf0ad97f26a29
Dylan <dylanmgaines1@gmail.com>
2021-04-22 15:39:53 -0400

    New Node Setup
    Set everything up in initial file. Server.js contains basic vars for express/axios

---
+++ server.js
@@ -0,0 +1,7 @@
+//fs, path, axios, express, body-parser, express-session
+let fs = require('fs');
+let path = require('path');
+let axios = require('axios');
+let express = require('express');
+let bodyParser = require('body-parser');
+let session = require('express-session');
